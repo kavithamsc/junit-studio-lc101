be6d360eec15898b7347ec51dd98bc0d13e00018
Kavitha <kavithamsca@gmail.com>
2023-06-19 18:34:30 -0500

    All unit test are passed

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -11,6 +12,68 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
-
-
+    @Test
+    public void  oneBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void twoBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+    @Test
+    public void bracketsWithinBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+@Test
+    public void stringBracketsReturnsTrue(){
+    assertTrue(BalancedBrackets.hasBalancedBrackets(" "));
+}
+@Test
+public void bracketsOfBracketsWithinBracketsReturnsTrue() {
+    assertTrue(BalancedBrackets.hasBalancedBrackets("[[]][]"));
+}
+    @Test
+    public void tripleBracketsWithinBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]]"));
+    }
+    @Test
+    public void twoTripleBracketsWithinBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]][]"));
+    }
+    @Test
+    public void launchCodeWithinBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[launchCode]"));
+    }
+    @Test
+    public void numbersWithinBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[1,2,3,4]"));
+    }
+    @Test
+    public void onlyOpenBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+    @Test
+    public void wordsWithOnlyOpenBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[Dog,Cat,Rat"));
+    }
+    @Test
+    public void onlyCloseBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+    @Test
+    public void numbersWithOnlyCloseBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("1,2,3]"));
+    }
+    @Test
+    public void misMatchesBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+    @Test
+    public void misMatchesWithinNumbersBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]1,2,3["));
+    }
+    @Test
+    public void misMatchesWithinWordsBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]Dog,Cat,Rat["));
+    }
 }
